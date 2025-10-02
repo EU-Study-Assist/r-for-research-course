9f380f516021e543c81497d0fd20e13ba8689d18
xrander <olamideadu@gmail.com>
2025-10-02 13:14:16 +0100

    file organization

--- scripts/01-flatfiles.R
+++ scripts/01-flatfiles.R
@@ -1,3 +1,18 @@
 # install.packages(tidyverse)
 library(tidyverse)
-read_table("data/adult.data", col_names = FALSE)
+
+read_table("data/adult_names/adult.data", col_names = FALSE) |>
+  set_names(
+    "age", "workclass", "fnlwgt", "education",
+    "education_num", "marital_status", "occupation",
+    "relationship", "race", "sex", "capital_gain",
+    "capital_loss", "hours_per_week", "native_country",
+    "lob_prob"
+  ) |>
+  select(-lob_prob) |>
+  write_tsv("data/flatfiles/adult_names.txt")
+
+
+read_table("data/adult_names/adult.test", col_names = FALSE)
+
+read_delim("data/flatfiles/plot_11_tvol.txt")
