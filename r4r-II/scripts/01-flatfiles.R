da605a74c6acab08054bb5e1c6836f20945f07e0
xrander <olamideadu@gmail.com>
2025-10-02 11:51:04 +0100

    data files update

--- r4r-II/scripts/01-flatfiles.R
+++ r4r-II/scripts/01-flatfiles.R
@@ -1,3 +1,18 @@
-# install.packages(tidyverse)
+# Importing TXT Files
+
+
+
 library(tidyverse)
-read_table("data/adult.data", col_names = FALSE)
+
+read_table("r4r-II/data/adult_names/adult.data", col_names = FALSE) |>
+  set_names(
+    "age", "workclass", "fnlwgt",
+    "education", "education-num",
+    "marital_status", "occupation", "relationship",
+    "race", "sex", "capital_gain", "capital_loss",
+    "hours_per_week", "native_country", "lab_prob"
+  ) |>
+  select(-lab_prob) |>
+  write_tsv("r4r-II/data/flatfiles/adult_data.txt")
+
+read_tsv("r4r-II/data/flatfiles/adult_data.txt")
